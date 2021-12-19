da6ad77b57d27506e3ad7b3dec4080871bdfee83
Patrick Dolan <dolanp1992@gmail.com>
2021-12-19 11:17:23 -0800

    refactor with beforeEach to create Character and add chooseClass describe

--- __test__/charactercreation.test.js
+++ __test__/charactercreation.test.js
@@ -1,32 +1,36 @@
 import { Character, rollD6 } from "./../src/charactercreation.js";
 
 describe("Character", () => {
+  let character;
+  beforeEach(() => {
+    character = new Character("John Smith");
+  });
   test("should correctly create a character object", () => {
-    let character = new Character("John Smith")
     expect(character.name).toBe("John Smith");
   });
   describe("generateStats", () => {
     test("should generate intelligence stat", () => {
-      let character = new Character("John Smith");
       character.generateStats()
       expect(character.stats.int).toBeGreaterThanOrEqual(1);
       expect(character.stats.int).toBeLessThan(7);
     });
     test("should generate strength stat", () => {
-      let character = new Character("John Smith");
       character.generateStats()
       expect(character.stats.str).toBeGreaterThanOrEqual(1);
       expect(character.stats.str).toBeLessThan(7);
     });
     test("should generate agility stat", () => {
-      let character = new Character("John Smith");
       character.generateStats()
       expect(character.stats.agi).toBeGreaterThanOrEqual(1);
       expect(character.stats.agi).toBeLessThan(7);
     });
   });
   describe("chooseClass", () => {
-    test("should take in ")
+    test("should take in a choice argument and add a class to the character", () => {
+      let choice = "Wizard";
+      character.chooseClass(choice);
+      expect(character.class).toEqual("Wizard");
+    });
   });
 });
 describe("rollD6", () => {
