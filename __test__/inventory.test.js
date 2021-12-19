cf7374850f2bab6385467f34d015ffa606521d8a
Patrick Dolan <dolanp1992@gmail.com>
2021-12-19 15:07:23 -0800

    add first test for Inventory constuctor
    Co-authored-by: Phil Curran <pecurran@hotmail.com>

--- __test__/inventory.test.js
+++ __test__/inventory.test.js
@@ -1,2 +1,10 @@
 import { Inventory } from "./../src/js/inventory.js";
 
+describe("Inventory", () => {
+
+  test("should initialize an inventory object", () => {
+    const inventory = new Inventory();
+    expect(inventory.items).toEqual([]);
+  });
+
+});
