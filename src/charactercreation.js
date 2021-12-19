b58743e70b26362cb45ded89a3d9d74037d46f8e
Patrick Dolan <dolanp1992@gmail.com>
2021-12-19 11:43:27 -0800

    add stat changes based class chosen to chooseClass function
    Co-authored-by: Phil Curran <pecurran@hotmail.com>

--- src/charactercreation.js
+++ src/charactercreation.js
@@ -15,6 +15,17 @@ export class Character {
   }
   chooseClass(choice) {
     this.class = choice;
+    switch (choice) {
+      case 'Wizard':
+        this.stats.int += 2;
+        break;
+      case 'Fighter':
+        this.stats.str += 2;
+        break;
+      case 'Rogue':
+        this.stats.agi += 2;
+        break;
+    }
   }
 }
 export function rollD6() {
