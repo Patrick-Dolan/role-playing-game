7ca1eeae6a2e124371b981a58ef9e8c4e0f7c953
Patrick Dolan <dolanp1992@gmail.com>
2021-12-19 10:34:26 -0800

    add test for rollD6() function

--- __test__/charactercreation.test.js
+++ __test__/charactercreation.test.js
@@ -1,8 +1,15 @@
-import { Character } from "./../src/charactercreation.js";
+import { Character, rollD6 } from "./../src/charactercreation.js";
 
 describe("Character", () => {
   test("should correctly create a character object", () => {
     let character = new Character("John Smith")
     expect(character.name).toBe("John Smith");
+  });
+});
+describe("rollD6", () => {
+  test("should return a number between 1 and 6", () => {
+    const randomNumber = rollD6();
+    expect(randomNumber).toBeGreaterThanOrEqualTo(1);
+    expect(randomNumber).toBeLessThan(7);
   });
 });
