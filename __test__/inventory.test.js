9e697ad46add2275fdf4873e0b7110bb5937981c
Patrick Dolan <dolanp1992@gmail.com>
2021-12-19 15:20:33 -0800

    add addItems function test
    Co-authored-by: Phil Curran <pecurran@hotmail.com>

--- __test__/inventory.test.js
+++ __test__/inventory.test.js
@@ -2,9 +2,22 @@ import { Inventory } from "./../src/js/inventory.js";
 
 describe("Inventory", () => {
 
+  let inventory;
+  beforeEach(() => {
+    inventory = new Inventory();
+  });
+
   test("should initialize an inventory object", () => {
-    const inventory = new Inventory();
     expect(inventory.items).toEqual([]);
+  });
+  
+  // Test to add items to inventory
+  describe("addItems", () => {
+    test("should add items to inventory.items", () => {
+      const item = "sword";
+      expect(inventory.items).toContain(item);
+    });
+
   });
 
 });
