72ec0e621b5bc8816361cf7bef56c0f4d6172e1d
Patrick Dolan <dolanp1992@gmail.com>
2021-12-21 20:23:58 -0800

    add stats change to levelUp function

--- src/js/levelsystem.js
+++ src/js/levelsystem.js
@@ -9,4 +9,18 @@ export function addExp(character) {
 }
 export function levelUp(character) {
   character.level += 1;
+  switch(character.level){
+    case 2:
+      character.stats.str += 1;
+      break;
+    case 3:
+      character.stats.str += 1;
+      break;
+    case 4:
+      character.stats.str += 1;
+      break;
+    case 5:
+      character.stats.str += 1;
+      break;
+  }
 }
