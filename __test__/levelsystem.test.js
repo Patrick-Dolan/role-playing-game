3e92bb4b6a1a6879ac491aac94ed37273208bec4
Patrick Dolan <dolanp1992@gmail.com>
2021-12-22 16:52:40 -0800

    add comments to make it easier to find specific functions

--- __test__/levelsystem.test.js
+++ __test__/levelsystem.test.js
@@ -9,17 +9,19 @@ beforeEach(() => {
   character.generateStats();
   character.chooseClass(classChoice);
 });
-
+// Check level function
 describe("checkLevel", () => {
   test("should return the level of the character", () => {
     expect(checkLevel(character)).toEqual(1);
   });
 });
+// Check Exp function
 describe("checkExp", () => {
   test("should return the exp of the character", () => {
     expect(checkExp(character)).toEqual(1);
   });
 });
+// AddExp function
 describe("addExp", () => {
   test("should add exp to the character based on 1 fights won", () => {
     character.fights = 1;
@@ -32,6 +34,7 @@ describe("addExp", () => {
     expect(character.exp).toEqual(25);
   });
 });
+// LevelUp function
 describe("levelUp", () => {
   test("should increase the strength of the character by 2", () => {
     let firstStr = character.stats.str;
