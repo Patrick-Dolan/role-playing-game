9a8f4c8363bfd3b2f2ec88a2cc1d9c19ce840de2
Patrick Dolan <dolanp1992@gmail.com>
2021-12-22 17:13:30 -0800

    add test for levelUp function change stats based on class

--- __test__/levelsystem.test.js
+++ __test__/levelsystem.test.js
@@ -3,11 +3,26 @@ import { checkLevel, checkExp, levelUp, addExp } from "../src/js/levelsystem";
 
 let fighterCharacter;
 let fighterChoice;
+let wizardCharacter;
+let wizardChoice;
+let rogueCharacter;
+let rogueChoice;
 beforeEach(() => {
+  //Fighter
   fighterCharacter = new Character("John Smith");
   fighterChoice = "Fighter";
   fighterCharacter.generateStats();
   fighterCharacter.chooseClass(fighterChoice);
+  //Wizard
+  wizardCharacter = new Character("John Smith");
+  wizardChoice = "Wizard";
+  wizardCharacter.generateStats();
+  wizardCharacter.chooseClass(wizardChoice);
+  //Rogue
+  rogueCharacter = new Character("John Smith");
+  rogueChoice = "Roguer";
+  rogueCharacter.generateStats();
+  rogueCharacter.chooseClass(rogueChoice);
 });
 // Check level function
 describe("checkLevel", () => {
@@ -37,14 +52,14 @@ describe("addExp", () => {
 // LevelUp function
 describe("levelUp", () => {
   // Fighter specific tests
-  test("should increase the strength of the character by 2 when character is level 2", () => {
+  test("should increase the strength of the character by 2 when character is a level 2 fighter", () => {
     let firstStr = fighterCharacter.stats.str;
     levelUp(fighterCharacter);
     expect(fighterCharacter.level).toEqual(2);
     expect(fighterCharacter.stats.str).toEqual(firstStr + 2);
   });
-  test("should increase the strength of the character by 2 when character is level 3", () => {
-    let firstStr = 0;fighterCharacter.stats.str;
+  test("should increase the strength of the character by 2 when character is a level 3 fighter", () => {
+    let firstStr = 0;
     for (let i = 0; i <= 1; i++){
       firstStr = fighterCharacter.stats.str;
       levelUp(fighterCharacter);
@@ -52,8 +67,8 @@ describe("levelUp", () => {
     expect(fighterCharacter.level).toEqual(3);
     expect(fighterCharacter.stats.str).toEqual(firstStr + 2);
   });
-  test("should increase the strength of the character by 2 when character is level 4", () => {
-    let firstStr = 0;fighterCharacter.stats.str;
+  test("should increase the strength of the character by 2 when character is a level 4 fighter", () => {
+    let firstStr = 0;
     for (let i = 0; i <= 2; i++){
       firstStr = fighterCharacter.stats.str;
       levelUp(fighterCharacter);
@@ -61,13 +76,81 @@ describe("levelUp", () => {
     expect(fighterCharacter.level).toEqual(4);
     expect(fighterCharacter.stats.str).toEqual(firstStr + 2);
   });
-  test("should increase the strength of the character by 2 when character is level 5", () => {
-    let firstStr = 0;fighterCharacter.stats.str;
+  test("should increase the strength of the character by 2 when character is a level 5 fighter", () => {
+    let firstStr = 0;
     for (let i = 0; i <= 3; i++){
       firstStr = fighterCharacter.stats.str;
       levelUp(fighterCharacter);
       }  
     expect(fighterCharacter.level).toEqual(5);
     expect(fighterCharacter.stats.str).toEqual(firstStr + 2);
+  });
+  // Wizard specific tests
+  test("should increase the intellgience of the character by 2 when character is a level 2 wizard", () => {
+    let firstInt = wizardCharacter.stats.int;
+    levelUp(wizardCharacter);
+    expect(wizardCharacter.level).toEqual(2);
+    expect(wizardCharacter.stats.int).toEqual(firstInt + 2);
+  });
+  test("should increase the intellgience of the character by 2 when character is a level 3 wizard", () => {
+    let firstInt = 0;
+    for (let i = 0; i <= 1; i++){
+      firstInt = wizardCharacter.stats.int;
+      levelUp(wizardCharacter);
+    }  
+    expect(wizardCharacter.level).toEqual(3);
+    expect(wizardCharacter.stats.int).toEqual(firstInt + 2);
+  });
+  test("should increase the intellgience of the character by 2 when character is a level 4 wizard", () => {
+    let firstInt = 0;
+    for (let i = 0; i <= 2; i++){
+      firstInt = wizardCharacter.stats.int;
+      levelUp(wizardCharacter);
+      }  
+    expect(wizardCharacter.level).toEqual(4);
+    expect(wizardCharacter.stats.int).toEqual(firstInt + 2);
+  });
+  test("should increase the intellgience of the character by 2 when character is a level 5 wizard", () => {
+    let firstInt = 0;
+    for (let i = 0; i <= 3; i++){
+      firstInt = wizardCharacter.stats.int;
+      levelUp(wizardCharacter);
+      }  
+    expect(wizardCharacter.level).toEqual(5);
+    expect(wizardCharacter.stats.int).toEqual(firstInt + 2);
+  });
+  // Rogue specific tests
+  test("should increase the agility of the character by 2 when character is a level 2 rogue", () => {
+    let firstAgi = rogueCharacter.stats.agi;
+    levelUp(rogueCharacter);
+    expect(rogueCharacter.level).toEqual(2);
+    expect(rogueCharacter.stats.agi).toEqual(firstAgi + 2);
+  });
+  test("should increase the agility of the character by 2 when character is a level 3 rogue", () => {
+    let firstAgi = 0;
+    for (let i = 0; i <= 1; i++){
+      firstAgi = rogueCharacter.stats.agi;
+      levelUp(rogueCharacter);
+    }  
+    expect(rogueCharacter.level).toEqual(3);
+    expect(rogueCharacter.stats.agi).toEqual(firstAgi + 2);
+  });
+  test("should increase the agility of the character by 2 when character is a level 4 rogue", () => {
+    let firstAgi = 0;
+    for (let i = 0; i <= 2; i++){
+      firstAgi = rogueCharacter.stats.agi;
+      levelUp(rogueCharacter);
+      }  
+    expect(rogueCharacter.level).toEqual(4);
+    expect(rogueCharacter.stats.agi).toEqual(firstAgi + 2);
+  });
+  test("should increase the agility of the character by 2 when character is a level 5 rogue", () => {
+    let firstAgi = 0;
+    for (let i = 0; i <= 3; i++){
+      firstAgi = rogueCharacter.stats.agi;
+      levelUp(rogueCharacter);
+      }  
+    expect(rogueCharacter.level).toEqual(5);
+    expect(rogueCharacter.stats.agi).toEqual(firstAgi + 2);
   });
 });
