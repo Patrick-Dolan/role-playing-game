7cd38d27e9d96c5821b1061c21be76f5c4605405
Patrick Dolan <dolanp1992@gmail.com>
2021-12-22 16:55:27 -0800

    change tests to be specific to fighterCharacter

--- __test__/levelsystem.test.js
+++ __test__/levelsystem.test.js
@@ -1,45 +1,45 @@
 import { Character } from "../src/js/charactercreation";
 import { checkLevel, checkExp, levelUp, addExp } from "../src/js/levelsystem";
 
-let character;
-let classChoice;
+let fighterCharacter;
+let fighterChoice;
 beforeEach(() => {
-  character = new Character("John Smith");
-  classChoice = "Fighter";
-  character.generateStats();
-  character.chooseClass(classChoice);
+  fighterCharacter = new Character("John Smith");
+  fighterChoice = "Fighter";
+  fighterCharacter.generateStats();
+  fighterCharacter.chooseClass(fighterChoice);
 });
 // Check level function
 describe("checkLevel", () => {
   test("should return the level of the character", () => {
-    expect(checkLevel(character)).toEqual(1);
+    expect(checkLevel(fighterCharacter)).toEqual(1);
   });
 });
 // Check Exp function
 describe("checkExp", () => {
   test("should return the exp of the character", () => {
-    expect(checkExp(character)).toEqual(1);
+    expect(checkExp(fighterCharacter)).toEqual(1);
   });
 });
 // AddExp function
 describe("addExp", () => {
   test("should add exp to the character based on 1 fights won", () => {
-    character.fights = 1;
-    addExp(character);
-    expect(character.exp).toEqual(5);
+    fighterCharacter.fights = 1;
+    addExp(fighterCharacter);
+    expect(fighterCharacter.exp).toEqual(5);
   });
   test("should add exp to the character based on 5 fights won", () => {
-    character.fights = 5;
-    addExp(character);
-    expect(character.exp).toEqual(25);
+    fighterCharacter.fights = 5;
+    addExp(fighterCharacter);
+    expect(fighterCharacter.exp).toEqual(25);
   });
 });
 // LevelUp function
 describe("levelUp", () => {
   test("should increase the strength of the character by 2", () => {
-    let firstStr = character.stats.str;
-    levelUp(character);
-    expect(character.level).toEqual(2);
-    expect(character.stats.str).toEqual(firstStr + 2);
+    let firstStr = fighterCharacter.stats.str;
+    levelUp(fighterCharacter);
+    expect(fighterCharacter.level).toEqual(2);
+    expect(fighterCharacter.stats.str).toEqual(firstStr + 2);
   });
 });
